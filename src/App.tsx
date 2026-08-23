7709ec7ee08279c20804d1f47639acc469000df6
Umar <umarislah86@gmail.com>
2026-08-23 19:05:04 +0800

    Fix routing: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import SetupPage from './pages/Setup/SetupPage';
 import BankerPage from './pages/Banker/BankerPage';
 import DisplayPage from './pages/Display/DisplayPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SetupPage />} />
         <Route path="/banker" element={<BankerPage />} />
         <Route path="/display" element={<DisplayPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
